2817a0212e4375a3201bb60cbe914d8bef0eeca4
Serhii-Hurhsal <seergiygurshall@gmail.com>
2023-05-31 11:40:08 +0300

    Added filters

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Map from "./components/Map";
+import Filters from "./components/Filters";
 
 function App() {
   return (
     <div className="App">
       <Map />
+      <Filters />
     </div>
   );
 }
---
+++ client/src/components/Filters.js
@@ -0,0 +1,33 @@
+import EducationIcon from "../icons/Education";
+import FoodAidIcon from "../icons/FoodAid";
+import GovernmentBodiesIcon from "../icons/GovernmentBodies";
+import IntegrationSupportOrganizationsIcon from "../icons/IntegrationSupportOrganizations";
+import LegalServicesIcon from "../icons/LegalServices";
+import LeisureIcon from "../icons/Leisure";
+import MedicalAssistanceIcon from "../icons/MedicalAssistance";
+import OralWrittenTranslationIcon from "../icons/OralWrittenTranslation";
+import OtherIcon from "../icons/Other";
+import PsychologicalAssistanceIcon from "../icons/PsychologicalAssistance";
+import TransportationIcon from "../icons/Transportation";
+import VolunteeringIcon from "../icons/Volunteering";
+
+const Filters = () => {
+  return (
+    <div className="filters">
+      <EducationIcon />
+      <FoodAidIcon />
+      <GovernmentBodiesIcon />
+      <IntegrationSupportOrganizationsIcon />
+      <LegalServicesIcon />
+      <LeisureIcon />
+      <MedicalAssistanceIcon />
+      <OralWrittenTranslationIcon />
+      <PsychologicalAssistanceIcon />
+      <TransportationIcon />
+      <VolunteeringIcon />
+      <OtherIcon />
+    </div>
+  );
+};
+
+export default Filters;
